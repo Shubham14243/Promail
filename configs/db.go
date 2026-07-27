63693a999591d786b5a80562002fe0df8fd6b649
shubham14243 <skgsmasher14243@gmail.com>
2026-07-27 21:51:45 +0530

    Fixed Pooling

--- configs/db.go
+++ configs/db.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"log"
 	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -17,6 +18,17 @@ func ConnectDB() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	// Disable server-side prepared statements.
+	// Required when running behind PgBouncer in transaction-pooling mode
+	// (or any pooler that rotates physical connections between transactions).
+	// Without this, lib/pq caches prepared statement names on a connection
+	// and fails with: pq: unnamed prepared statement does not exist (26000)
+	// when the connection is rotated to a different backend.
+	db.SetMaxOpenConns(25)
+	db.SetMaxIdleConns(25)
+	db.SetConnMaxLifetime(5 * time.Minute)
+	db.SetConnMaxIdleTime(2 * time.Minute)
 
 	if err = db.Ping(); err != nil {
 		log.Fatal(err)
